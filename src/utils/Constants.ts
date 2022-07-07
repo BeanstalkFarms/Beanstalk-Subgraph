fe01431426ba5d20efcc3e178e919b000d57878c
cujowolf <cujowolf12@gmail.com>
2022-07-07 15:30:12 -0500

    Initial Silo schema entities for replant

---
+++ src/utils/Constants.ts
@@ -0,0 +1,14 @@
+import { Address } from "@graphprotocol/graph-ts"
+
+// Standard Addresses
+export const ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
+
+// Token Addresses
+export const BEAN_ERC20 = Address.fromString('0xDC59ac4FeFa32293A95889Dc396682858d52e5Db')
+
+// Protocol Addresses
+
+// LP Addresses
+
+// Other Constants
+export const BEAN_DECIMALS = 6
