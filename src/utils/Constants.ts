a5f66062162e50a120704b35f54c96cd01176729
publius <beanstalk.publius@protonmail.com>
2022-08-05 20:06:07 -0400

    Fertilizer

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,4 +1,4 @@
-import { Address } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal } from "@graphprotocol/graph-ts"
 
 // Standard Addresses
 export const ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
@@ -9,8 +9,13 @@ export const BEAN_ERC20 = Address.fromString('0xDC59ac4FeFa32293A95889Dc39668285
 // Protocol Addresses
 export const BEANSTALK = Address.fromString('0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5')
 export const CURVE_PRICE = Address.fromString('0xA57289161FF18D67A68841922264B317170b0b81')
+export const FERTILIZER = Address.fromString('0x402c84De2Ce49aF88f5e2eF3710ff89bFED36cB6')
 
 // LP Addresses
 
 // Other Constants
 export const BEAN_DECIMALS = 6
+
+export const INITIAL_HUMIDITY = BigDecimal.fromString('500')
+export const MIN_HUMIDITY = BigDecimal.fromString('500')
+export const DELTA_HUMIDITY = BigDecimal.fromString('0.5')
