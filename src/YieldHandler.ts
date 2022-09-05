e7a8ca86a3838faad3a55d044244d34708aed1a3
Silo Chad <silochad@protonmail.com>
2022-09-05 14:07:20 -0500

    Add comments, remove division by 100

--- src/YieldHandler.ts
+++ src/YieldHandler.ts
@@ -42,33 +42,55 @@ export function updateBeanEMA(t: i32): void {
 
 function calculateAPY(n: BigDecimal, seeds: BigDecimal): BigDecimal {
     // This iterates through 8760 times to calculate the silo APY
-
     let silo = loadSilo(BEANSTALK)
 
-    let C = toDecimal(silo.totalSeeds)
-    let K = toDecimal(silo.totalStalk, 10)
-    let b = seeds.div(BigDecimal.fromString('2'))
-    let k = BigDecimal.fromString('1')
-
+    // Initialize sequence
+    let C = toDecimal(silo.totalSeeds)              // Init: Total Seeds
+    let K = toDecimal(silo.totalStalk, 10)          // Init: Total Stalk
+    let b = seeds.div(BigDecimal.fromString('2'))   // Init: User BDV
+    let k = BigDecimal.fromString('1')              // Init: User Stalk
     let b_start = b
 
-    let C_1 = ZERO_BD
-    let K_1 = ZERO_BD
-    let b_1 = ZERO_BD
-    let k_1 = ZERO_BD
+    // Placeholders for above values during each iteration
+    let C_i = ZERO_BD
+    let K_i = ZERO_BD
+    let b_i = ZERO_BD
+    let k_i = ZERO_BD
+
+    // Stalk and Seeds per Deposited Bean.
+    let STALK_PER_SEED = BigDecimal.fromString('0.0001'); // 1/10,000 Stalk per Seed
+    let STALK_PER_BEAN = BigDecimal.fromString('0.0002'); // 2 Seeds per Bean * 1/10,000 Stalk per Seed
 
     for (let i = 0; i < 8760; i++) {
-        C_1 = C.plus((BigDecimal.fromString('2').times(n)))
-        b_1 = b.plus((n.times((k.div(K)))))
-        k_1 = k.plus((n.times((k.div(K))))).plus(BigDecimal.fromString('0.0002').times(b))
-        K_1 = K.plus(n).plus(BigDecimal.fromString('0.0001').times(C))
+        // Each Season, Farmer's ownership = `current Stalk / total Stalk`
+        let ownership = k.div(K)
+        let newBeans  = n.times(ownership)
+        
+        // Total Seeds: each seignorage Bean => 2 Seeds
+        C_i = C.plus(n.times(BigDecimal.fromString('2')))
+        // Total Stalk: each seignorage Bean => 1 Stalk, each outstanding Bean => 1/10_000 Stalk
+        K_i = K.plus(n/* .times(1) */).plus(STALK_PER_SEED.times(C))
+        // User Beans: each seignorage Bean => 1 BDV
+        b_i = b.plus(newBeans)
+        // User Stalk: each seignorage Bean => 1 Stalk, each outstanding Bean => d = 1/5_000 Stalk per Bean
+        k_i = k.plus(newBeans).plus(STALK_PER_BEAN.times(b))
 
-        C = C_1
-        K = K_1
-        b = b_1
-        k = k_1
+        C = C_i
+        K = K_i
+        b = b_i
+        k = k_i
     }
 
-    let siloAPY = (b.minus(b_start)).div(BigDecimal.fromString('100'))
+    // Examples:
+    // -------------------------------
+    // b_start = 1
+    // b       = 1
+    // b.minus(b_start) = 0   = 0% APY
+    //
+    // b_start = 1
+    // b       = 1.1
+    // b.minus(b_start) = 0.1 = 10% APY
+    let siloAPY = b.minus(b_start)
+
     return siloAPY
 }
